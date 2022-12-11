34c4e8be39a0c71ef954914613c47ee8e8bbe15b
Aakash-Rana <rana.aaki@gmail.com>
2022-12-11 11:16:45 +0530

    MVC pattern with static files

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const app=express();
+const morgan = require('morgan');
 
-const PORT=8484;
-app.listen(PORT,()=>{
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-app.get("/",(req,res)=>{
-    res.status(200).send("WELCOME TO MY PAGE");
-})
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;

f50244f844391ef049b3a8e3a95575d544701b1f
Aakash-Rana <rana.aaki@gmail.com>
2023-03-14 23:23:05 +0530

    Prepared app for deployment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -55,6 +56,10 @@ app.use(
     ]
   })
 );
+
+// To compress the text response
+app.use(compression());
+
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
